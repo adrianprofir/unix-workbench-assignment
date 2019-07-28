6e4841cf544563b422dd0b9875a3f4a7b5a5a22e
adrianprofir <adrian.profir95@gmail.com>
2019-07-28 10:58:40 +0300

    added the makefile

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,14 +5,14 @@ userguess=0
 function checknumber {
 	if [[ $1 -lt $2 ]]
 	then
-	echo "You have entered a lower number"
+	echo "echo You have entered a lower number"
 	elif [[ $1 -gt $2 ]]
 	then
-	echo "You have entered a higher number"
+	echo "echo You have entered a higher number"
 	fi
 }
 
-while [ $userguess -ne $nfiles]
+while [ $userguess -ne $nfiles ]
 do
 echo "Enter you guess for the number of files!"
 read userguess
