d89e4e81819a8d023f28274a86b492f331837ff1
adrianprofir <adrian.profir95@gmail.com>
2019-07-28 10:52:24 +0300

    created the script

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,24 @@
 numfiles=(*)
 nfiles=${#numfiles[@]}
-echo $nfiles
+userguess=0
+
+function checknumber {
+	if [[ $1 -lt $2 ]]
+	then
+	echo "You have entered a lower number"
+	elif [[ $1 -gt $2 ]]
+	then
+	echo "You have entered a higher number"
+	fi
+}
+
+while [ $userguess -ne $nfiles]
+do
+echo "Enter you guess for the number of files!"
+read userguess
+
+$(checknumber $userguess $nfiles)
+
+done
+
+echo "You got the right answer!"
